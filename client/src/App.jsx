72624ea1d0dc03209d6a6a283539cc038031b117
Saurabh kumar <1213saurabhsingh1213@gmail.com>
2025-03-09 01:06:44 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <Button>lets bulid lms</Button>
+      <p class="bg-green-400">hello world</p>
+    </>
+  )
+}
+
+export default App
